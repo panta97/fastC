31f7b8c3f2278ae85ebacaa1ac70bd911a92f364
Roosevelt <panta_85.x@hotmail.com>
2018-02-01 20:41:53 -0500

    basic cuadre in

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,15 +10,62 @@ export class MainComponent implements OnInit {
   centimos: number;
   result: string;
 
+  unidad = {
+    diez: null,
+    vein: null,
+    unso: null,
+    doso: null,
+    cinco: null,
+    diezso: null,
+    veinso: null,
+    cincuso: null,
+    cienso: null,
+    dosciso: null
+  }
+
+  denom = {
+    diez: 0.1,
+    vein: 0.2,
+    unso: 1,
+    doso: 2,
+    cinco: 5,
+    diezso: 10,
+    veinso: 20,
+    cincuso: 50,
+    cienso: 100,
+    dosciso: 200
+  }
+
+  monto = {
+    '0.1': '0.00',
+    '0.2': '0.00',
+    '1': '0.00',
+    '2': '0.00',
+    '5': '0.00',
+    '10': '0.00',
+    '20': '0.00',
+    '50': '0.00',
+    '100': '0.00',
+    '200': '0.00'
+  }
+
   constructor() { }
 
   ngOnInit() {
     
   }
 
-  calcular() {
-    this.result = (0.1 * this.centimos).toFixed(1);
-    console.log(this.result)
+  calcular(denom, unidad) {
+    this.monto[denom] = (denom * unidad).toFixed(2);
+    // console.log(this.result)
+    console.log(this.monto['0.1'])
+  }
+
+  clearAll() {
+    for(var key in this.unidad) {
+      this.unidad[key] = null;
+      this.calcular(this.denom[key], 0);
+    }
   }
 
 }
